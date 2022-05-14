fcf8cd7d833c68a154e3d017a72e1d05570d9ef8
Christian Larson <larsonchristian505@gmail.com>
2022-05-14 08:14:22 -0700

    6 kyu 10 min walk codewars

---
+++ 6th kyu/10 minute walk.js
@@ -0,0 +1,34 @@
+function isValidWalk(walk) {
+
+    //check if walk fulfills 10 minute requirement
+    if (walk.length != 10) {
+        return false
+    }
+
+    //count the number of each direction, and add them to an object
+    let dirTally = walk.reduce((prev, curr) => {
+        if (typeof prev[curr] == 'undefined') {
+            prev[curr] = 1
+            return prev
+        }
+        else {
+            prev[curr] ++
+            return prev
+        }
+    }, {})
+    console.log(dirTally)
+
+    //if each direction appears the same amount of times as its opposite, we have arrived back at our starting location
+    if (dirTally.n != dirTally.s || dirTally.w != dirTally.e) {
+        return false
+    }
+
+    else {
+        return true
+    }
+}
+
+
+isValidWalk(['n','s','n','s','n','s','n','s','n','s'])
+
+
