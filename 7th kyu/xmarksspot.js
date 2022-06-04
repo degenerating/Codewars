ba2cc6cc33ec9e0565c7b63194e613b9678fb986
Christian Larson <larsonchristian505@gmail.com>
2022-06-04 09:07:48 -0700

    7k

---
+++ 7th kyu/xmarksspot.js
@@ -0,0 +1,81 @@
+// #'x' marks the spot
+
+// ##Task:
+
+// Given a two dimensional array, return the co-ordinates of 'x'.
+
+// If 'x' is not inside the array, or if 'x' appears multiple times, return []
+
+// The co-ordinates should be zero indexed.
+
+// You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+// Example test cases:
+
+// 'Return an empty array if input is an empty array' => []
+
+// [] 
+
+// 'Return an empty array if no x found' => []
+
+// [
+//   ['o', 'o'],
+//   ['o', 'o']
+// ]
+
+// 'Return an empty array if more than one x found' => []
+
+// [
+//   ['x', 'o'],
+//   ['o', 'x']
+// ]
+
+// 'Return [0,0] when x at top left' => [0, 0]
+
+// [
+//   ['x', 'o'],
+//   ['o', 'o']
+// ]
+
+// 'Return [4,6] for the example below' => [4, 6]
+
+// [
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+// ]
+
+
+const xMarksTheSpot = (input) => {
+    if (input == []) {
+        return []
+    }
+    result = 0
+
+    for (i = 0; i < input.length; i++) {
+        for (x = 0; x < input[i].length; x++) {
+            if (input[i][x] == 'x') {
+                if (result == 0) {
+                    result = [i, x]
+                }
+                else {
+                    return []
+                }
+            }
+        }
+    }
+    console.log(result)
+    if (result == 0) {
+        return []
+    }
+    return result
+  }
+
+
+xMarksTheSpot([
+    ['x', 'o'],
+    ['o', 'o']
+  ])
