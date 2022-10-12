efd0ddc32846b8b920dd83eb4e269a8dc8178a86
Christian Larson <larsonchristian505@gmail.com>
2022-10-12 09:08:30 -0700

    revised 10

--- huntober/huntober10.js
+++ huntober/huntober10.js
@@ -1,12 +1,17 @@
 function rearrange(arr) {
+
+    let origArr = []
+    origArr = origArr.concat(arr)
+
+
     let leftShifted = []
     let rightShifted = []
 
     let result = []
 
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i].includes('a')) {
-            leftShifted.push(arr.splice(i, 1))
+    for (i = 0; i < origArr.length; i++) {
+        if (origArr[i].includes('a')) {
+            leftShifted.push(origArr.splice(i, 1))
             i--
             // console.log(`leftshifted: ${leftShifted}`)
             // console.log(`arr: ${arr}`)
@@ -17,9 +22,9 @@ function rearrange(arr) {
 
 
     //maybe switch away from using unshift for memory reasons. instead iterate backwards using push
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i].length > 3) {
-            rightShifted.push(arr.splice(i, 1))
+    for (i = 0; i < origArr.length; i++) {
+        if (origArr[i].length > 3) {
+            rightShifted.push(origArr.splice(i, 1))
             i--
             // console.log(`rightshifted: ${rightShifted}`)
             // console.log(`arr: ${arr}`)
@@ -35,7 +40,7 @@ function rearrange(arr) {
     //done, seems to be fine
 
 
-    result = result.concat(arr)
+    result = result.concat(origArr)
 
     result = result.concat(rightShifted)
 
@@ -51,3 +56,5 @@ let myArr = ['hi', 'hello', 'howdy', 'hola', 'hej', 'hallo', 'heyyy']
 
 
 rearrange(myArr)
+
+console.log(myArr)
